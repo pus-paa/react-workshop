3586be4baa6a576b3777913fbc82164807bcf4bb
puspa <puspabh@gmail.com>
2023-12-31 10:44:15 +0545

    day 2 assignment 2

--- src/ClickCounter.jsx
+++ src/ClickCounter.jsx
@@ -1,27 +1,22 @@
-import react,{useState} from "react";
+import react, { useState } from "react";
 
-const ClickCounter=() =>{
-    const[counterOne,setCounterOne]=useState(0);
-    const increment=()=>{
-        setCounterOne(counterOne+1);
+const ClickCounter = () => {
+  const [counterOne, setCounterOne] = useState(0);
+  const increment = () => {
+    setCounterOne(counterOne + 1);
+  };
+  const decrement = () => {
+    setCounterOne(counterOne - 2);
+  };
 
-
-    };
-    const decrement=()=>{
-        setCounterOne(counterOne -2);
-    };   
-
-    return(
-        <div>
-            <h1>ClickCounter</h1>
-            <h2>Count is{counterOne}</h2>
-            <button onClick={increment}>click me to increase</button>
-            <button onClick={decrement}>Click me to decrease by 2</button>
-            <button onClick={decrement}>Click me to decrease by -5</button>
-
-        </div>
-
-    )
-
-}
+  return (
+    <div>
+      <h1>ClickCounter</h1>
+      <h2>Count is{counterOne}</h2>
+      <button onClick={increment}>click me to increase</button>
+      <button onClick={decrement}>Click me to decrease by 2</button>
+      <button onClick={decrement}>Click me to decrease by -5</button>
+    </div>
+  );
+};
 export default ClickCounter;
